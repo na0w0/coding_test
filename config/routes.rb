8ebf42b40203487a61c2a5b8c9369c399c8789bc
Nao Yukimori <sutoradaf2@gmail.com>
2021-09-15 21:00:44 +0900

    ルーティングを変更articles→users/articles

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,11 @@ Rails.application.routes.draw do
   root 'top#index'
   devise_for :users
 
-  # 一般ユーザー
-  # 特定のユーザーの記事一覧と詳細
   resources :users do
-    resources :articles, only: %w(index show)
+    resources :articles
   end
 
   # ログインユーザーの記事一覧
   # TODO: to: 'users#show'かindexにする
   get 'mypage', to: 'articles#index'
-  resources :articles
 end
