6dd0ed90523e31a9722beed46faa2de5d7b5ce00
Nao Yukimori <sutoradaf2@gmail.com>
2021-09-27 09:30:12 +0900

    production以外でエラーページを表示しないよう変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   add_flash_types :success, :info, :warning, :danger
 
-  include ErrorHandlers
+  include ErrorHandlers if Rails.env.production?
   
   private
 
