bd411ec587814397afb5dda066f259ddae957994
Nao Yukimori <sutoradaf2@gmail.com>
2021-09-30 00:09:57 +0900

    最新の投稿を表示するように修正

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,6 +1,6 @@
 class MypageController < ApplicationController
     before_action :authenticate_user!
     def index
-        @posts = Post.where(user_id: current_user.id)
+        @posts = Post.where(user_id: current_user.id).order(created_at: 'DESC')
     end
 end
