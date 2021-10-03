8e638ca6a7525da045c7a71badc249ddbf468a96
Nao Yukimori <sutoradaf2@gmail.com>
2021-10-03 23:26:17 +0900

    rubocopの警告対応

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'Comments', type: :request do
   let!(:user) { create(:user) }
-  let!(:other_user) { create(:user) }
+  let(:other_user) { create(:user) }
   let!(:new_post) { create(:post, user_id: user.id) }
-  let!(:new_comment) { create(:comment, post_id: new_post.id, user_id: user.id)}
-  let!(:comment_params) { attributes_for(:comment, post_id: new_post.id, user_id: user.id) }
+  let!(:new_comment) { create(:comment, post_id: new_post.id, user_id: user.id) }
+  let(:comment_params) { attributes_for(:comment, post_id: new_post.id, user_id: user.id) }
+
   describe 'POST #create' do
     context 'ログインユーザーの場合' do
       before do
@@ -14,8 +15,9 @@ RSpec.describe 'Comments', type: :request do
 
       it 'リクエストが成功すること' do
         post post_comments_path(post_id: new_post.id, comment: comment_params)
-        expect(response).to have_http_status(302)
+        expect(response).to have_http_status(:found)
       end
+
       it '記事の詳細ページに遷移すること' do
         post post_comments_path(post_id: new_post.id, comment: comment_params)
         expect(response).to redirect_to user_post_path(id: new_comment.post.id, user_id: new_comment.post.user.id)
@@ -31,14 +33,14 @@ RSpec.describe 'Comments', type: :request do
 
       it 'リクエストが成功すること' do
         get edit_post_comment_path(id: new_comment.id, post_id: new_post.id)
-        expect(response).to have_http_status(200)
+        expect(response).to have_http_status(:ok)
       end
     end
 
     describe 'ユーザーがログインしていない場合' do
       it 'リクエストが成功すること' do
         get edit_post_comment_path(id: new_comment.id, post_id: new_post.id)
-        expect(response).to have_http_status(302)
+        expect(response).to have_http_status(:found)
       end
     end
   end
@@ -51,7 +53,7 @@ RSpec.describe 'Comments', type: :request do
 
       it 'リクエストが成功すること' do
         put post_comment_path(post_id: new_comment.post.id, id: new_comment.id, comment: comment_params)
-        expect(response).to have_http_status(302)
+        expect(response).to have_http_status(:found)
       end
 
       it '記事の詳細ページに遷移すること' do
@@ -65,8 +67,9 @@ RSpec.describe 'Comments', type: :request do
     describe 'ユーザーがログインしていない場合' do
       it 'リクエストが成功すること' do
         put post_comment_path(post_id: new_comment.post.id, id: new_comment.id, comment: comment_params)
-        expect(response).to have_http_status(302)
+        expect(response).to have_http_status(:found)
       end
+
       it 'ログインページへ遷移されること' do
         put post_comment_path(post_id: new_comment.post.id, id: new_comment.id, comment: comment_params)
         expect(response).to redirect_to :new_user_session
@@ -82,7 +85,7 @@ RSpec.describe 'Comments', type: :request do
 
       it 'リクエストが成功すること' do
         delete post_comment_path(id: new_comment.id, post_id: new_comment.post.id)
-        expect(response).to have_http_status(302)
+        expect(response).to have_http_status(:found)
       end
 
       it '記事が削除されること' do
