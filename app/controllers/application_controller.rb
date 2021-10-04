7426bc3ac136ecb6d5e1c45b1b9ab49555417314
NaoYukimori <sutoradaf2@gmail.com>
2021-10-04 20:11:06 +0900

    [PR]エラーハンドラ関連のレビュー対応 (#5)
    * rubocopのコードフォーマットを適応
    
    * h3タグからh1タグに変更
    
    * インラインで書いていたstyleをscssに移動
    
    * rubocopのコードフォーマットを適応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
 
   include ErrorHandlers if Rails.env.production?
-  
+
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
 end
