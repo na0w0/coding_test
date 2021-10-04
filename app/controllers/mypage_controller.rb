259767abe949c8c2f00aad32f41aeb11d51d5b76
NaoYukimori <sutoradaf2@gmail.com>
2021-10-05 00:05:31 +0900

    [PR]マイページ関連のレビュー対応 (#6)
    * インデントを修正
    
    * current_userからpostsを取得するよう修正
    
    * rubocopのコードフォーマットを適応
    
    * karminariをインストール
    
    * ページネーションの処理を追加
    
    * kaminariのconfigを追加
    
    * kaminariをgitのリポジトリを指定してインストール
    
    * karminariのbootstrapテーマを適応
    
    * kaminariのロケールファイルを追加

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,6 +1,6 @@
 class MypageController < ApplicationController
-    before_action :authenticate_user!
-    def index
-        @posts = Post.where(user_id: current_user.id).order(created_at: 'DESC')
-    end
+  before_action :authenticate_user!
+  def index
+    @posts = current_user.posts.order(created_at: :desc).page(params[:page])
+  end
 end
