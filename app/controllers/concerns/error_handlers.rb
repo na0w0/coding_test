2f39f6bab3eaa955aec03429452aa27b06ef7518
Nao Yukimori <sutoradaf2@gmail.com>
2021-10-04 19:22:21 +0900

    rubocopのコードフォーマットを適応

--- app/controllers/concerns/error_handlers.rb
+++ app/controllers/concerns/error_handlers.rb
@@ -1,18 +1,18 @@
 module ErrorHandlers
-    extend ActiveSupport::Concern
+  extend ActiveSupport::Concern
 
-    included do
-        rescue_from StandardError, with: :rescue500
-        rescue_from ActiveRecord::RecordNotFound, with: :rescue404
-    end
+  included do
+    rescue_from StandardError, with: :rescue500
+    rescue_from ActiveRecord::RecordNotFound, with: :rescue404
+  end
 
-    private
+  private
 
-    def rescue404(e)
-        render 'errors/not_found', status: 404
-    end
+  def rescue404
+    render 'errors/not_found', status: :not_found
+  end
 
-    def rescue500(e)
-        render 'errors/internal_server_error', status: 500
-    end
+  def rescue500
+    render 'errors/internal_server_error', status: :internal_server_error
+  end
 end
