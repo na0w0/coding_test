92f66bccd86d6f77352cb9ac9dcb83d4c4a7b8bf
Nao Yukimori <sutoradaf2@gmail.com>
2021-10-04 18:39:16 +0900

    テストを追加

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -5,12 +5,30 @@ RSpec.describe 'Comments', type: :request do
   let(:other_user) { create(:user) }
   let!(:new_post) { create(:post, user_id: user.id) }
   let!(:new_comment) { create(:comment, post_id: new_post.id, user_id: user.id) }
-  let(:comment_params) { attributes_for(:comment, post_id: new_post.id, user_id: user.id) }
+  let!(:other_comment) { create(:comment, post_id: new_post.id, user_id: other_user.id) }
+  let(:comment_params) { attributes_for(:comment) }
+  let(:other_comment_params) { attributes_for(:comment) }
 
   describe 'POST #create' do
     context 'ログインユーザーの場合' do
       before do
         login_as user
+      end
+
+      it 'リクエストが成功すること' do
+        post post_comments_path(post_id: new_post.id, comment: comment_params)
+        expect(response).to have_http_status(:found)
+      end
+
+      it '記事の詳細ページに遷移すること' do
+        post post_comments_path(post_id: new_post.id, comment: comment_params)
+        expect(response).to redirect_to user_post_path(id: new_comment.post.id, user_id: new_comment.post.user.id)
+      end
+    end
+
+    context '異なるログインユーザーの場合' do
+      before do
+        login_as other_user
       end
 
       it 'リクエストが成功すること' do
@@ -35,12 +53,29 @@ RSpec.describe 'Comments', type: :request do
         get edit_post_comment_path(id: new_comment.id, post_id: new_post.id)
         expect(response).to have_http_status(:ok)
       end
+
+      it '自分の記事の異なるユーザーのコメントが編集できること' do
+        get edit_post_comment_path(id: other_comment.id, post_id: new_post.id)
+        expect(response).to have_http_status(:ok)
+      end
     end
 
-    describe 'ユーザーがログインしていない場合' do
+    context 'ユーザーがログインしていない場合' do
       it 'リクエストが成功すること' do
         get edit_post_comment_path(id: new_comment.id, post_id: new_post.id)
         expect(response).to have_http_status(:found)
+      end
+    end
+
+    context '異なるログインユーザーの場合' do
+      before do
+        login_as other_user
+      end
+
+      it 'リクエストが失敗すること' do
+        expect {
+          get edit_post_comment_path(id: new_comment.id, post_id: new_post.id)
+        }.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
   end
@@ -57,14 +92,19 @@ RSpec.describe 'Comments', type: :request do
       end
 
       it '記事の詳細ページに遷移すること' do
-        # put post_comment_path(post_id: new_comment.post.id, id: new_comment.id, comment: comment_params)
         comment_params[:content] = 'sample'
         put post_comment_path(id: new_comment.id, post_id: new_comment.post_id, comment: comment_params)
-        expect(response).to redirect_to user_post_path(new_comment.post)
+        expect(response).to redirect_to user_post_path(id: new_comment.post.id, user_id: new_comment.user.id)
+      end
+
+      it '自分の記事の異なるユーザーのコメントを更新できること' do
+        other_comment_params[:content] = 'sample'
+        put post_comment_path(id: other_comment.id, post_id: other_comment.post_id, comment: other_comment_params)
+        expect(response).to have_http_status(:found)
       end
     end
 
-    describe 'ユーザーがログインしていない場合' do
+    context 'ユーザーがログインしていない場合' do
       it 'リクエストが成功すること' do
         put post_comment_path(post_id: new_comment.post.id, id: new_comment.id, comment: comment_params)
         expect(response).to have_http_status(:found)
@@ -73,6 +113,18 @@ RSpec.describe 'Comments', type: :request do
       it 'ログインページへ遷移されること' do
         put post_comment_path(post_id: new_comment.post.id, id: new_comment.id, comment: comment_params)
         expect(response).to redirect_to :new_user_session
+      end
+    end
+
+    context '異なるログインユーザーの場合' do
+      before do
+        login_as other_user
+      end
+
+      it 'リクエストが失敗すること' do
+        expect {
+          get edit_post_comment_path(id: new_comment.id, post_id: new_post.id)
+        }.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
   end
@@ -92,6 +144,24 @@ RSpec.describe 'Comments', type: :request do
         expect do
           delete post_comment_path(id: new_comment.id, post_id: new_comment.post.id)
         end.to change(Comment, :count).by(-1)
+      end
+
+      it '自分の記事の異なるユーザーのコメントが削除できること' do
+        expect do
+          delete post_comment_path(id: other_comment.id, post_id: other_comment.post.id)
+        end.to change(Comment, :count).by(-1)
+      end
+    end
+
+    context '異なるログインユーザーの場合' do
+      before do
+        login_as other_user
+      end
+
+      it 'リクエストが失敗すること' do
+        expect {
+          get edit_post_comment_path(id: new_comment.id, post_id: new_post.id)
+        }.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
   end
