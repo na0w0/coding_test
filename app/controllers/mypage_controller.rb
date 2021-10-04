e23afe506fdbcbd82724f1830d649f2afdede77d
Nao Yukimori <sutoradaf2@gmail.com>
2021-10-04 23:32:44 +0900

    kaminariのconfigを追加

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,6 +1,6 @@
 class MypageController < ApplicationController
   before_action :authenticate_user!
   def index
-    @posts = current_user.posts.order(created_at: 'DESC').page(params[:page]).per(5)
+    @posts = current_user.posts.order(created_at: :desc).page(params[:page])
   end
 end
