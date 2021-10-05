d0a8cb750e60480120be5ac6e3fa044dd94b1a93
Nao Yukimori <sutoradaf2@gmail.com>
2021-10-05 16:19:08 +0900

    postテーブルにnot null制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_03_150901) do
+ActiveRecord::Schema.define(version: 2021_10_05_071734) do
 
   create_table "comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "content", null: false
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2021_10_03_150901) do
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id"
     t.string "title"
-    t.text "content"
+    t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
