07b42db929a7930d344fcd825de0777d1375a152
Nao Yukimori <sutoradaf2@gmail.com>
2021-10-05 21:49:11 +0900

    投稿ユーザーのチェック処理をヘルパーメソッド化

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [ :index, :show ]
+  helper_method :post_contributor?, :comment_contributor?
 
   def index
     @user = User.find(params[:user_id])
@@ -48,5 +49,13 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :content)
+  end
+
+  def post_contributor?
+    user_signed_in? && current_user.id == @post.user.id
+  end
+
+  def comment_contributor?(comment)
+    user_signed_in? && current_user.id == comment.user.id || current_user.id == comment.post.user.id
   end
 end
